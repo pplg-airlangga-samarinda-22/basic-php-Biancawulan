7efc54536d032841d15f2f18f7ae257ece72a953
Biancawulan <binabinra@gmail.com>
2025-01-07 09:07:02 +0800

    selesai

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
+// simpan variable nama dan NISN
+$nama = "Muhammad Firdaus Bina Rajendra";
+$nisn = "0077446978";
 
+//Tampilkan nama dan NISN ke layar
+echo "Nama: " . $nama . "<br>";
+echo "NISN: " . $nisn;
 ?>
